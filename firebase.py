59a6d595aff37d07809e154eea139ba2496f9575
lindsayh17 <lhall11@uvm.edu>
2026-01-28 16:11:20 -0500

    Separate connection stuff into a separate file

--- firebase.py
+++ firebase.py
@@ -1,13 +1,4 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-
-# Use a service account.
-cred = credentials.Certificate('serviceAccountKey.json')
-
-app = firebase_admin.initialize_app(cred)
-
-db = firestore.client()
+from connectionAuthentication import db
 
 #actual database reference
 countries_ref = db.collection("countries")
