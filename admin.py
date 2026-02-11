d1a818d6cacb8b91e9ca6df1be82126137161b25
lindsayh17 <lhall11@uvm.edu>
2026-02-10 19:36:32 -0500

    Admin file documentation

--- admin.py
+++ admin.py
@@ -1,25 +1,25 @@
 # Admin program
 '''
 The admin program will read data from a JSON file saved locally and will initialize and upload the data
-to a Google Firebase Cloud Datastore (not a Firebase Realtime Database). You’ll run this program one
-time. If you run it a second time, it should delete and recreate the datastore
+to a Google Firebase Cloud Datastore (not a Firebase Realtime Database). It takes a single command-line
+argument: the name of the JSON file containing the data to load.
 '''
 import sys
-
-'''
-Your admin program should take a single command-line argument: the name of the JSON file containing
-the data to load. So for example, I would run my program this way:
-
-$ python admin.py restaurant-data.json
-
-The admin program will then read and parse the JSON file and upload the data to your Firebase
-datastore. If the datastore already contains data, then the existing data will first be deleted.
-'''
 import json
 from connectionAuthentication import db
 
 class Country:
     def __init__(self, country, region, area, population, gdp, coastline=None):
+        """
+        Country constructor
+
+        :param country: string
+        :param region: string
+        :param area: number
+        :param population: number
+        :param gdp: number
+        :param coastline: number
+        """
         self.country = country.strip()
         self.region = region
         self.area = area
@@ -29,9 +29,20 @@ class Country:
 
     @classmethod
     def from_dict(cls, source):
+        """
+        Convert dictionary to a country
+
+        :param source: dictionary
+        :return: Country object
+        """
         return cls(source['Country'], source['Region'], source['Area'], source['Population'], source['GDP'], source.get('Coastline'))
 
     def to_dict(self):
+        """
+        Convert country to a dictionary
+
+        :return: dictionary
+        """
         dictionary = {
             'Region': self.region,
             'Area': self.area,
@@ -39,6 +50,7 @@ class Country:
             'GDP': self.gdp,
         }
 
+        # only input coastline if it has a value
         if self.coastline != 0:
             dictionary['Coastline'] = self.coastline
 
@@ -54,12 +66,15 @@ class Country:
                 coastline={self.coastline}\
             )"
 
-# TODO: delete current docs first?
 def populate_firebase(source):
+    """
+    Put countries into firebase
+
+    :param source: json file of countries
+    """
     try:
         with open(source, 'r') as f:
             country_data = json.load(f)
-
 
         collection_reference = db.collection("countries")
         for country_dict in country_data:
